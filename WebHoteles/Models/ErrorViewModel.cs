4a4fed065b9bd18b539aab40e6c2608320ded80c
Darkencio311 <rebollito311@gmail.com>
2025-05-29 12:39:07 -0500

    New Files

---
+++ WebHoteles/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebHoteles.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ WebHoteles/Models/Usuarios.cs
@@ -0,0 +1,13 @@
+﻿namespace WebHoteles.Models
+{
+    public class Usuarios
+    {
+
+        public int Id { get; set; }
+
+        public string CorreoUsuario { get; set; }
+
+        public string ContraseñaUsuario  { get; set; }
+     }
+}
+        
