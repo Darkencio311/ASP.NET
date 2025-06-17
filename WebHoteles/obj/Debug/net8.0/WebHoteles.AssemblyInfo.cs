abe9e7ca7ce50928eff60cecde01586886c75de6
Darkencio311 <rebollito311@gmail.com>
2025-06-16 19:21:54 -0500

    Agrego un nuevo archivo txt

--- WebHoteles/obj/Debug/net8.0/WebHoteles.AssemblyInfo.cs
+++ WebHoteles/obj/Debug/net8.0/WebHoteles.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebHoteles")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4a4fed065b9bd18b539aab40e6c2608320ded80c")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebHoteles")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebHoteles")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
